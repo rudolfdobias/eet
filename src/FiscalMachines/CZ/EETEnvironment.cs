525fcf7d622a6581ec24c07e91264f85d61acd22
Jiří Helmich <jiri@helmich.cz>
2016-10-07 14:51:45 +0200

    Initial commit.¨

---
+++ src/FiscalMachines/CZ/EETEnvironment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiscalMachines.CZ
+{
+    public enum EETEnvironment
+    {
+        Production,
+        Playground
+    }
+}
