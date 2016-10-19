70ed14ce67dbc14f8802425c5876c30f1b854a0e
Jiří Helmich <jiri@helmich.cz>
2016-10-19 18:37:45 +0200

    assembly info rename

--- src/Mews.Eet.Tests/Properties/AssemblyInfo.cs
+++ src/Mews.Eet.Tests/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FiscalMachines.CZ.Tests")]
+[assembly: AssemblyTitle("Mews.Eet.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FiscalMachines.CZ.Tests")]
+[assembly: AssemblyProduct("Mews.Eet.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- src/Mews.Eet/Properties/AssemblyInfo.cs
+++ src/Mews.Eet/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("eetcz")]
+[assembly: AssemblyTitle("Mews.Eet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("eetcz")]
+[assembly: AssemblyProduct("Mews.Eet")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
