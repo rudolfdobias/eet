0f87e13e2fa250d9ba037db50e6b46d9b4e0825f
Jiří Helmich <jiri@helmich.cz>
2016-11-11 14:46:11 +0100

    v0.2.0 - Basic logging.

--- src/Mews.Eet/EetClient.cs
+++ src/Mews.Eet/EetClient.cs
@@ -6,17 +6,34 @@ namespace Mews.Eet
 {
     public class EetClient
     {
-        public EetClient(Certificate certificate, EetEnvironment environment = EetEnvironment.Production)
+        public EetClient(Certificate certificate, EetEnvironment environment = EetEnvironment.Production, ILogger logger = null)
         {
             EetSoapClient = new EetSoapClient(certificate, environment);
+            Logger = logger;
         }
 
         private EetSoapClient EetSoapClient { get; }
 
-        public Task<SendRevenueResult> SendRevenueAsync(RevenueRecord record, EetMode mode = EetMode.Operational)
+        private ILogger Logger { get; }
+
+        public async Task<SendRevenueResult> SendRevenueAsync(RevenueRecord record, EetMode mode = EetMode.Operational)
         {
-            var task = EetSoapClient.SendRevenue(new SendRevenueMessage(record, mode).GetXmlMessage());
-            return task.ContinueWith(t => Task.FromResult(new SendRevenueResult(t.Result))).Unwrap();
+            Logger?.Info($"Sending bill {record.BillNumber} to EET servers in {mode} mode.");
+
+            var xmlMessage = new SendRevenueMessage(record, mode).GetXmlMessage();
+            var sendRevenueResult = await EetSoapClient.SendRevenueAsync(xmlMessage).ConfigureAwait(continueOnCapturedContext: false);
+            var result = new SendRevenueResult(sendRevenueResult);
+
+            if (result.IsError)
+            {
+                Logger?.Info($"Got error response from EET servers for bill {record.BillNumber}.");
+            }
+            else
+            {
+                Logger?.Info($"Got success response from EET servers for bill {record.BillNumber}.");
+            }
+
+            return result;
         }
     }
 }
---
+++ src/Mews.Eet/ILogger.cs
@@ -0,0 +1,9 @@
+﻿namespace Mews.Eet
+{
+    public interface ILogger
+    {
+        void Error(string message);
+        void Info(string message);
+        void Debug(string message);
+    }
+}
